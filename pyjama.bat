c43364146fc837386dd638b3b87cc98ec9fdb222
Doug Blank <dblank@brynmawr.edu>
2011-01-07 00:52:24 +0000

    pause, mono path hard-coded

--- pyjama.bat
+++ pyjama.bat
@@ -1,1 +1,3 @@
-"c:\Program Files (x86)\Mono-2.8\bin\mono.exe" bin\ipy.exe src\pyjama.py 
+"c:\Program Files (x86)\Mono-2.8.1\bin\mono" bin\ipy.exe src\pyjama.py
+;; "c:\Program Files (x86)\Mono-2.8\bin\mono.exe" bin\ipy.exe src\pyjama.py
+pause
