8dcf8dc99fb5d6bb2fade02a4899ff6411c1cfe7
Doug Blank <dblank@brynmawr.edu>
2011-01-14 00:32:14 +0000

    Move startup files to pyjama.exe

--- pyjama.bat
+++ pyjama.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Loading Pyjama...
 cd %HOMEDRIVE%\%HOMEPATH%\Pyjama\
-"c:\Program Files (x86)\Mono-2.8.1\bin\mono" bin\ipy.exe src\pyjama.py %*
-REM "c:\Program Files (x86)\Mono-2.8\bin\mono.exe" bin\ipy.exe src\pyjama.py %*
+set MONO_PATH=bin
+"c:\Program Files (x86)\Mono-2.8.1\bin\mono" pyjama.exe %*
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    pause
