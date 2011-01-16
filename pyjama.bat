e45500c54d124b46fa58286d2b64a58fde31ff8b
Doug Blank <dblank@brynmawr.edu>
2011-01-16 21:10:03 +0000

    Windows version is restricted to 2.6.4 if you want to run Dinah due to drag-and-drop bugs. Reported to Mono: https://bugzilla.novell.com/show_bug.cgi?id=664824

--- pyjama.bat
+++ pyjama.bat
@@ -1,7 +1,9 @@
 @echo off
 echo Loading Pyjama...
 cd %HOMEDRIVE%\%HOMEPATH%\Pyjama\
-"c:\Program Files (x86)\Mono-2.8.1\bin\mono" bin\pyjama.exe %*
+"c:\Program Files (x86)\Mono-2.6.4\bin\mono" bin\pyjama.exe %*
+REM "c:\Program Files (x86)\Mono-2.8.2\bin\mono" bin\pyjama.exe %*
+REM "c:\Program Files (x86)\Mono-2.6.7\bin\mono" bin\pyjama.exe %*
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    pause
