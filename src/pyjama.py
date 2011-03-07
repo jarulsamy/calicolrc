1f05f7a1ed7ed73b6d9d593a43fabc73a2177da7
Doug Blank <dblank@brynmawr.edu>
2011-03-07 17:59:06 +0000

    Version 0.3.6

--- src/pyjama.py
+++ src/pyjama.py
@@ -575,7 +575,7 @@ class PyjamaProject(object):
 
 
 # Let's start!
-version = "0.3.5"
+version = "0.3.6"
 args = sys.argv[1:] or list(System.Environment.GetCommandLineArgs())[1:]
 if "--help" in args:
     print
