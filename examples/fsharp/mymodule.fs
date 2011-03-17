cefb2167ddd12fab0966a2b5e264043060cb5bfb
Doug Blank <dblank@brynmawr.edu>
2011-03-17 12:59:12 +0000

    fsharp example

---
+++ examples/fsharp/mymodule.fs
@@ -0,0 +1,7 @@
+module MyModule
+
+Graphics.Init()
+let win = new Graphics.Window("Hello")
+let line = new Graphics.Line(new Graphics.Point(0, 0),
+                             new Graphics.Point(100, 100))
+line.draw(win)
