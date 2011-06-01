7ea798187ea7d5a7d47ba48cd4fe1376af04f365
Doug Blank <dblank@brynmawr.edu>
2011-06-01 15:18:06 +0000

    Clean up on Levenshtein

--- examples/python/levenshtein.py
+++ examples/python/levenshtein.py
@@ -1,24 +1,48 @@
-def memoize(f):
-    def memoized_f(x, y):
-        if (x,y) not in cache:
-            cache[(x, y)] = f(x, y)
-        return cache[(x, y)]
-    return memoized_f
+import time
+def timer(f, *args):
+    start = time.time()
+    retval = f(*args)
+    print "Time:", time.time() - start
+    return retval
 
-def levenshteinDistance(s1, s2):
+def ld0(s1, s2):
+    if len(s1) == 0: return len(s2)
+    if len(s2) == 0: return len(s1)
+    if s1[0] == s2[0]:
+        return min( ld0(s1[1:], s2[1:]) + 0,
+                    ld0(s1, s2[1:]) + 1,
+                    ld0(s1[1:], s2) + 1)
+    else:
+        return min( ld0(s1[1:], s2[1:]) + 1,
+                    ld0(s1, s2[1:]) + 1,
+                    ld0(s1[1:], s2) + 1)
+
+def ld1(s1, s2):
+    if len(s1) == 0: return len(s2)
+    if len(s2) == 0: return len(s1)
+    cost = 0 if (s1[0] == s2[0]) else 1
+    return min( ld1(s1[1:], s2[1:]) + cost,
+                ld1(s1, s2[1:]) + 1,
+                ld1(s1[1:], s2) + 1)
+
+def memoize(f):
+    def m(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    return m
+
+@memoize
+def ld2(s1, s2):
+    if len(s1) == 0: return len(s2)
+    if len(s2) == 0: return len(s1)
+    cost = 0 if (s1[0] == s2[0]) else 1
+    return min( ld2(s1[1:], s2[1:]) + cost,
+                ld2(s1, s2[1:]) + 1,
+                ld2(s1[1:], s2) + 1)
+
+def ld3(s1, s2):
     global cache
     cache = {}
-    m = len(s1)
-    n = len(s2)
-    cache = {}
-    @memoize
-    def dist(m, n):
-        if m == 0: return n
-        if n == 0: return m
-        cost = 0 if (s1[m - 1] == s2[n - 1]) else 1
-        return min( dist(m - 1, n - 1) + cost,
-                    dist(m, n - 1) + 1,
-                    dist(m - 1, n) + 1)
-    return dist(m, n)
+    return ld2(s1, s2)
 
-ld = levenshteinDistance
