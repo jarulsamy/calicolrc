ef1e9020a4ecb76824fcda6cae483b2bebe1b0f0
Doug Blank <dblank@brynmawr.edu>
2011-06-17 12:14:51 +0000

    Added Arc and Pie examples

---
+++ examples/python/arc.py
@@ -0,0 +1,4 @@
+from Graphics import *
+win = Window()
+a = Arc((150, 150), 50, 0, 90)
+a.draw(win)
---
+++ examples/python/pie.py
@@ -0,0 +1,6 @@
+from Graphics import *
+win = Window()
+a = Pie((150, 150), 50, 0, 90)
+a.fill = Color("red")
+a.border = 3
+a.draw(win)
