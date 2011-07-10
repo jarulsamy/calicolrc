c4d72bd574c3d69fbd8c0de2d51daa4af0bd8d01
Doug Blank <dblank@brynmawr.edu>
2011-07-10 22:43:47 +0000

    Physics of letters

---
+++ examples/python/bouncetext2.py
@@ -0,0 +1,24 @@
+from Graphics import *
+
+win = Window("Hello World!", 640, 360)
+win.mode = "physics"
+
+line = 0
+for text in ["Hello", "World!", "Welcome", "to", "Calico"]:
+    col = 0
+    for letter in text:
+        t = Text((320 + col, 50 + line), letter)
+        t.draw(win)
+        t.wrap = True
+        col += 10
+    line += 16
+
+ground = Rectangle((100, 340), (500, 360))
+ground.draw(win)
+ground.bodyType = "static"
+ground.color = Color("green")
+
+for i in range(20):
+    win.step()
+getMouse()
+win.run()
