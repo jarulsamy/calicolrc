99596a05c9120b40a4a75469f82fcb30e10ad5f7
Doug Blank <dblank@brynmawr.edu>
2011-08-05 04:50:56 +0000

    Added SDL.dll for windows in bin directory

--- StartCalico.bat
+++ StartCalico.bat
@@ -7,6 +7,7 @@ cd /d "%~dp0"
 REM Run calico.exe with mono in a particular language:
 REM SET LANGUAGE=es_ES.UTF8
 REM SET LANG=es_ES.UTF8
+SET PATH=%PATH%;%CD%\bin
 "C:\Program Files (x86)\Mono-2.6.7\bin\mono" bin\calico.exe %*
 REM Report an error, if one, and pause to let user see it:
 if errorlevel 1 (
