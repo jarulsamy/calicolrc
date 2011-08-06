18d80fbadeacd9d6f98adf4bd014e207d7d88eb5
Doug Blank <dblank@brynmawr.edu>
2011-08-06 01:47:04 +0000

    version 0.5.7

--- src/calico.py
+++ src/calico.py
@@ -636,7 +636,7 @@ class CalicoProject(object):
 
 
 # Let's start!
-version = "0.5.6"
+version = "0.5.7"
 if "--help" in args:
     print()
     print(_("Calico Project, Version %s, on %s") % (version,
