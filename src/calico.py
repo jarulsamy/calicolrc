9432bffb6ef999142e6c8beecc3f997fb465b9f1
Doug Blank <dblank@brynmawr.edu>
2011-08-25 12:23:09 +0000

    Trunk is now 2.0.0; new branches/calico1 for stable

--- src/calico.py
+++ src/calico.py
@@ -621,7 +621,7 @@ class CalicoProject(object):
             actionHandler(action, **data)
 
 # Let's start!
-version = "1.0.0"
+version = "2.0.0"
 if "--help" in args:
     print()
     print(_("Calico Project, Version %s, on %s") % (version,
