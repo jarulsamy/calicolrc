ca9a66355be3c4ea53583809cac3c989d091419d
Doug Blank <dblank@brynmawr.edu>
2011-08-26 12:33:12 +0000

    Updated example

--- examples/python/curvemouse.py
+++ examples/python/curvemouse.py
@@ -2,10 +2,10 @@ from Graphics import *
 win = Window("Shapes", 200, 200)
 curve = None
 
-while True:
-    mouse = Point(*getMouseNow())
+while win.IsRealized:
+    mouse = getMouseNow()
     if curve:
         curve.undraw()
-    curve = Curve((10, 10), (mouse.x, mouse.y), (mouse.x, mouse.y), (190, 190))
+    curve = Curve((10, 10), mouse, mouse, (190, 190))
     curve.border = 3
     curve.draw(win)
