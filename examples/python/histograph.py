007e8fdfc6f5b73639cea1f3bd6791b48ba7ce4c
Doug Blank <dblank@brynmawr.edu>
2011-09-22 23:16:51 +0000

    histograph working example

--- examples/python/histograph.py
+++ examples/python/histograph.py
@@ -1,17 +1,14 @@
 from Graphics import *
 
-left = Picture(640, 480, Color("blue"))
-#left.fill = Color("blue")
+left = Picture("../images/statue-2.png")
+right = Picture("../images/statue-orig.png")
 
-right = Picture(640, 480, Color("red"))
-#right.fill =
-
-win = Window("Histograph", 640, 520)
+win = Window("Histograph", left.width, left.height + 40)
 image = copyPicture(right)
-image.center = Point(320, 240)
+image.center = Point(left.width/2, left.height/2)
 image.draw(win)
 
-slider = HSlider((0, 480), 640)
+slider = HSlider((0, left.height), left.width)
 slider.draw(win)
 
 last = 0
@@ -23,7 +20,7 @@ def redisplay(o, e):
         if region:
             region.undraw()
         if new > 0:
-            region = left.getRegion((last/100 * left.width,0), new/100 * left.width, left.height, 0)
+            region = left.getRegion((0,0), new/100 * left.width, left.height)
             region.draw(win)
         last = new
 
