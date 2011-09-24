cf10e811ff1d7d09bda11d984a06e43c6bb8ec11
Doug Blank <dblank@brynmawr.edu>
2011-09-24 01:45:02 +0000

    Updated example

--- examples/python/bitmap.py
+++ examples/python/bitmap.py
@@ -1,5 +1,5 @@
 from Graphics import *
-from Myro import random
+from Myro import random, getColorNames, pickOne, wait
 
 win = Window()
 win.mode = "bitmap"
@@ -7,6 +7,8 @@ win.mode = "bitmap"
 while True:
     x1, y1, x2, y2 = [random() * 300 for i in range(4)]
     line = Line((x1, y1), (x2, y2))
+    line.color = Color(pickOne(getColorNames()))
     line.draw(win)
-print(len(win.canvas.shapes))
+    wait(.01)
+#print(len(win.canvas.shapes))
 
