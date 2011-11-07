73c57bcd200185a9daf3bdf9d1bf25ccbd85930e
Doug Blank <dblank@brynmawr.edu>
2011-11-07 04:57:41 +0000

    New DLL for Calico2

---
+++ languages/Jigsaw/CalicoJigsaw.cs
@@ -0,0 +1,58 @@
+//
+//  CalicoJigsaw.cs
+//  
+//  Author:
+//       Douglas S. Blank <dblank@cs.brynmawr.edu>
+// 
+//  Copyright (c) 2011 The Calico Project
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Calico;
+
+public class CalicoJigsawEngine : Engine {
+    public CalicoJigsawEngine(LanguageManager manager) : base(manager) {
+    }
+}
+
+public class CalicoJigsawDocument : Document {
+	public Jigsaw.Canvas cvs = null;
+	
+	public CalicoJigsawDocument(Calico.MainWindow calico, string filename) : base(calico, filename, "jigsaw") {
+		cvs = new Jigsaw.Canvas(900, 600);
+		widget.AddWithViewport(cvs);
+        widget.ShowAll();
+	}
+}
+
+public class CalicoJigsawLanguage : Language {
+	public CalicoJigsawLanguage() : 
+		base("jigsaw",  "Jigsaw", new string[] { "jig", "xml" }, null) {
+    }
+	
+	public override void MakeEngine(LanguageManager manager) {
+        engine = new CalicoJigsawEngine(manager);
+    }
+
+	public override Document MakeDocument(Calico.MainWindow calico, string filename) {
+          return new CalicoJigsawDocument(calico, filename);
+    }
+
+    public static new Language MakeLanguage() {
+        return new CalicoJigsawLanguage();
+    }
+}
