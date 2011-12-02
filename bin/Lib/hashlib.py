dd4d57e01c84af4850bfb015d52303fd3795da7d
Doug Blank <dblank@brynmawr.edu>
2011-12-02 14:01:56 +0000

    Updates for IronPython 2.7

--- bin/Lib/hashlib.py
+++ bin/Lib/hashlib.py
@@ -1,93 +1,146 @@
-# Copyright (c) 2006 Seo Sanghyeon
+# $Id$
+#
+#  Copyright (C) 2005   Gregory P. Smith (greg@krypto.org)
+#  Licensed to PSF under a Contributor Agreement.
+#
 
-# 2006-01-26 sanxiyn Created as md5.py
-# 2006-03-25 sanxiyn Copied md5.py as sha.py
-# 2006-10-11 sanxiyn Reorganized to follow hashlib API
-#                    Added digest_size
-# 2006-11-11 sanxiyn Accept array as well as string
+__doc__ = """hashlib module - A common interface to many hash functions.
 
-from System.Security.Cryptography import (
-    HashAlgorithm,
-    MD5, SHA1, SHA256, SHA384, SHA512)
+new(name, string='') - returns a new hash object implementing the
+                       given hash function; initializing the hash
+                       using the given string data.
 
-from System.Text import Encoding
-raw = Encoding.GetEncoding('iso-8859-1')
-empty = raw.GetBytes('')
+Named constructor functions are also available, these are much faster
+than using new():
 
-from System import Array, Byte
-import array
+md5(), sha1(), sha224(), sha256(), sha384(), and sha512()
 
-class PythonHash:
+More algorithms may be available on your platform but the above are
+guaranteed to exist.
 
-    def __init__(self, context):
-        self.context = context
-        self.digest_size = context.HashSize / 8
+NOTE: If you want the adler32 or crc32 hash functions they are available in
+the zlib module.
 
-    def update(self, string):
-        if isinstance(string, array.array):
-            string = string.array
-        if isinstance(string, str):
-            bytes = raw.GetBytes(string)
-        elif isinstance(string, Array[Byte]):
-            bytes = string
-        elif isinstance(string, Array):
-            bytes = Array[Byte](list(string))
-        else:
-            raise TypeError("argument must be string or array")
-        self.context.TransformBlock(bytes, 0, len(bytes), bytes, 0)
+Choose your hash function wisely.  Some have known collision weaknesses.
+sha384 and sha512 will be slow on 32 bit platforms.
 
-    def digest(self):
-        context = self.context.MemberwiseClone()
-        context.TransformFinalBlock(empty, 0, 0)
-        return raw.GetString(context.Hash)
+Hash objects have these methods:
+ - update(arg): Update the hash object with the string arg. Repeated calls
+                are equivalent to a single call with the concatenation of all
+                the arguments.
+ - digest():    Return the digest of the strings passed to the update() method
+                so far. This may contain non-ASCII characters, including
+                NUL bytes.
+ - hexdigest(): Like digest() except the digest is returned as a string of
+                double length, containing only hexadecimal digits.
+ - copy():      Return a copy (clone) of the hash object. This can be used to
+                efficiently compute the digests of strings that share a common
+                initial substring.
 
-    def hexdigest(self):
-        context = self.context.MemberwiseClone()
-        context.TransformFinalBlock(empty, 0, 0)
-        return ''.join(['%02x' % byte for byte in context.Hash])
+For example, to obtain the digest of the string 'Nobody inspects the
+spammish repetition':
 
-    def copy(self):
-        context = self.context.MemberwiseClone()
-        return PythonHash(context)
+    >>> import hashlib
+    >>> m = hashlib.md5()
+    >>> m.update("Nobody inspects")
+    >>> m.update(" the spammish repetition")
+    >>> m.digest()
+    '\\xbbd\\x9c\\x83\\xdd\\x1e\\xa5\\xc9\\xd9\\xde\\xc9\\xa1\\x8d\\xf0\\xff\\xe9'
 
-# As of Mono 1.1.17.1, HashAlgorithm.Create doesn't accept lowercased
-# algorithm names unlike .NET. Mono bug #79641.
+More condensed:
 
-def new(name, string=''):
-    name = name.upper()
-    context = HashAlgorithm.Create(name)
-    if not context:
-        raise ValueError('unsupported hash type')
-    hash = PythonHash(context)
-    if string:
-        hash.update(string)
-    return hash
+    >>> hashlib.sha224("Nobody inspects the spammish repetition").hexdigest()
+    'a4337bc45a8fc544c03f52dc550cd6e1e87021bc896588bd79e901e2'
 
-def make_new(algorithm):
-    def new(string=''):
-        context = algorithm.Create()
-        hash = PythonHash(context)
-        if string:
-            hash.update(string)
-        return hash
-    return new
+"""
 
-md5 = make_new(MD5)
-sha1 = make_new(SHA1)
-sha256 = make_new(SHA256)
-sha384 = make_new(SHA384)
-sha512 = make_new(SHA512)
+# This tuple and __get_builtin_constructor() must be modified if a new
+# always available algorithm is added.
+__always_supported = ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
 
-def sha224(string=''):
-    raise NotImplementedError(
-        ".NET Framework doesn't provide SHA224 hash algorithm")
+algorithms = __always_supported
+
+__all__ = __always_supported + ('new', 'algorithms')
+
+
+def __get_builtin_constructor(name):
+    try:
+        if name in ('SHA1', 'sha1'):
+            import _sha
+            return _sha.new
+        elif name in ('MD5', 'md5'):
+            import _md5
+            return _md5.new
+        elif name in ('SHA256', 'sha256', 'SHA224', 'sha224'):
+            import _sha256
+            bs = name[3:]
+            if bs == '256':
+                return _sha256.sha256
+            elif bs == '224':
+                return _sha256.sha224
+        elif name in ('SHA512', 'sha512', 'SHA384', 'sha384'):
+            import _sha512
+            bs = name[3:]
+            if bs == '512':
+                return _sha512.sha512
+            elif bs == '384':
+                return _sha512.sha384
+    except ImportError:
+        pass  # no extension module, this hash is unsupported.
+
+    raise ValueError('unsupported hash type %s' % name)
+
+
+def __get_openssl_constructor(name):
+    try:
+        f = getattr(_hashlib, 'openssl_' + name)
+        # Allow the C module to raise ValueError.  The function will be
+        # defined but the hash not actually available thanks to OpenSSL.
+        f()
+        # Use the C function directly (very fast)
+        return f
+    except (AttributeError, ValueError):
+        return __get_builtin_constructor(name)
+
+
+def __py_new(name, string=''):
+    """new(name, string='') - Return a new hashing object using the named algorithm;
+    optionally initialized with a string.
+    """
+    return __get_builtin_constructor(name)(string)
+
+
+def __hash_new(name, string=''):
+    """new(name, string='') - Return a new hashing object using the named algorithm;
+    optionally initialized with a string.
+    """
+    try:
+        return _hashlib.new(name, string)
+    except ValueError:
+        # If the _hashlib module (OpenSSL) doesn't support the named
+        # hash, try using our builtin implementations.
+        # This allows for SHA224/256 and SHA384/512 support even though
+        # the OpenSSL library prior to 0.9.8 doesn't provide them.
+        return __get_builtin_constructor(name)(string)
+
 
 try:
-    import clr
-    clr.AddReference('Mono.Security')
-except:
-    pass
-else:
-    # Use Mono's SHA224 implementation when available
-    from Mono.Security.Cryptography import SHA224
-    sha224 = make_new(SHA224)
+    import _hashlib
+    new = __hash_new
+    __get_hash = __get_openssl_constructor
+except ImportError:
+    new = __py_new
+    __get_hash = __get_builtin_constructor
+
+for __func_name in __always_supported:
+    # try them all, some may not work due to the OpenSSL
+    # version not supporting that algorithm.
+    try:
+        globals()[__func_name] = __get_hash(__func_name)
+    except ValueError:
+        import logging
+        logging.exception('code for hash %s was not found.', __func_name)
+
+# Cleanup locals()
+del __always_supported, __func_name, __get_hash
+del __py_new, __hash_new, __get_openssl_constructor
