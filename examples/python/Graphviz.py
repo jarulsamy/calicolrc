b31ab61448873f64e771534331dcb5bb16477e71
Doug Blank <dblank@brynmawr.edu>
2012-02-05 01:22:21 +0000

    All examples working

--- examples/python/Graphviz.py
+++ examples/python/Graphviz.py
@@ -4,9 +4,9 @@ hello_world = """
 digraph {
         node [label="\\N"];
         graph [bb="0,0,74,112"];
-        Hello [label="<left> | Hello | <right>", pos="37,93", width="0.91667", height="0.52778"];
+        Hello [pos="37,93", width="0.91667", height="0.52778"];
         World [pos="37,19", width="1.0278", height="0.52778"];
-        Hello:left -> World [pos="e,37,38.249 37,73.943 37,66.149 37,56.954 37,48.338"];
+        Hello -> World [pos="e,37,38.249 37,73.943 37,66.149 37,56.954 37,48.338"];
 }
 """
 
@@ -156,10 +156,10 @@ tree = """digraph {
         17 [label="<left> | 17 | <right>", pos="104,19", rects="65.5,1,86.5,37 86.5,1,121.5,37 121.5,1,142.5,37", width="1.0556", height="0.51389"];
         14:left -> 7 [pos="e,58.226,185.32 73,223 73,212.77 68.994,202.66 63.861,193.9"];
         14:right -> 26 [pos="e,143.77,185.32 129,223 129,212.77 133.01,202.66 138.14,193.9"];
-        7 -> 12 [pos="e,52.328,111.06 66,149 66,138.84 62.264,128.62 57.521,119.76"];
-        26 -> 20 [pos="e,132,111.16 132,149 132,140.01 132,130.22 132,121.37"];
-        26 -> 31 [pos="e,205.67,111.19 188,149 188,138.14 192.96,127.82 199.29,119.06"];
-        20 -> 17 [pos="e,104,37.159 104,75 104,66.015 104,56.218 104,47.367"];
+        7:right -> 12 [pos="e,52.328,111.06 66,149 66,138.84 62.264,128.62 57.521,119.76"];
+        26:left -> 20 [pos="e,132,111.16 132,149 132,140.01 132,130.22 132,121.37"];
+        26:right -> 31 [pos="e,205.67,111.19 188,149 188,138.14 192.96,127.82 199.29,119.06"];
+        20:left -> 17 [pos="e,104,37.159 104,75 104,66.015 104,56.218 104,47.367"];
 }
 """
 
@@ -167,10 +167,10 @@ count = 1
 for content, options in [(hello_world, {"label": "Hello World",
                                         "default_shape" : "circle",
                                         }),
-                          (fsm, {"label": "Finite State Machine", "default_shape":"doublecircle"}),
-                          (kennedys, {"label": "Kennedys", "default_shape": "circle", "width": 800}),
-                          (phil, {"default_shape":"box"}),
-                          (tree, {"label": "Binary Tree", "default_shape" : "box", "line_type": "line"})
+                         (fsm, {"label": "Finite State Machine", "default_shape":"doublecircle"}),
+                         (kennedys, {"label": "Kennedys", "default_shape": "circle", "width": 800}),
+                         (phil, {"default_shape":"box"}),
+                         (tree, {"label": "Binary Tree", "default_shape" : "box", "line_type": "line"})
                         ]:
     g = Graphics.Graph()
     g.load(content)
