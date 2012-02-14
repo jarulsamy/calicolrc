05ac822a56f27268eb81269f961c7735af5abd97
Doug Blank <dblank@brynmawr.edu>
2012-02-14 22:21:44 +0000

    Fixed, and refined, but still C

--- examples/python/re.py
+++ examples/python/re.py
@@ -6,33 +6,33 @@ def grep(regexp, text):
 ## match: search for regexp anywhere in text ##
 def match(regexp, text):
     if (regexp[0] == '^'):
-        return matchhere(regexp[1:], text)
+        return match_here(regexp[1:], text)
     while text:
         ## must look even if string is empty ##
-        if (matchhere(regexp, text)):
+        if (match_here(regexp, text)):
             return True
         text = text[1:]
     return False
 
-## matchhere: search for regexp at beginning of text ##
-def matchhere(regexp, text):
+## match_here: search for regexp at beginning of text ##
+def match_here(regexp, text):
   if (regexp[0] == '\0'):
     return True
   if (regexp[1] == '*'):
-    return matchstar(regexp[0], regexp[2:], text)
+    return match_star(regexp[0], regexp[2:], text)
   if (regexp[0] == '$' and regexp[1] == '\0'):
     return (text == '\0')
   if (text != '\0' and (regexp[0] == '.' or regexp[0] == text[0])):
-    return matchhere(regexp[1:], text[1:])
+    return match_here(regexp[1:], text[1:])
   return False
 
-## matchstar: search for c*regexp at beginning of text ##
-def matchstar(c, regexp, text):
+## match_star: search for c*regexp at beginning of text ##
+def match_star(c, regexp, text):
     do = True
     pos = 0
     while do:
         ## a * matches zero or more instances ##
-        if (matchhere(regexp, text)):
+        if (match_here(regexp, text[pos:])):
             return True
         do = (text[pos] != '\0' and (text[pos] == c or c == '.'))
         pos += 1
