a9eca975b918cff150120e11888f313feb8c2ec4
Doug Blank <dblank@brynmawr.edu>
2012-02-22 04:32:08 +0000

    Added Math.dll

---
+++ modules/Math/Math.cs
@@ -0,0 +1,212 @@
+using System;
+
+public class Math {
+
+    public const double PI = System.Math.PI;
+    public const double E = System.Math.E;
+
+    public static System.Int64 Abs (Int64 value) {
+        return System.Math.Abs(value);
+    }
+    public static System.SByte Abs (SByte value) {
+        return System.Math.Abs(value);
+    }
+    public static System.Int16 Abs (Int16 value) {
+        return System.Math.Abs(value);
+    }
+    public static System.Int32 Abs (Int32 value) {
+        return System.Math.Abs(value);
+    }
+    public static System.Decimal Abs (Decimal value) {
+        return System.Math.Abs(value);
+    }
+    public static double Abs (double value) {
+        return System.Math.Abs(value);
+    }
+    public static System.Single Abs (Single value) {
+        return System.Math.Abs(value);
+    }
+    public static double Acos (double d) {
+        return System.Math.Acos(d);
+    }
+    public static double Asin (double d) {
+        return System.Math.Asin(d);
+    }
+    public static double Atan (double d) {
+        return System.Math.Atan(d);
+    }
+    public static double Atan2 (double y, double x) {
+        return System.Math.Atan2(y, x);
+    }
+    public static System.Int64 BigMul (Int32 a, Int32 b) {
+        return System.Math.BigMul(a, b);
+    }
+    public static double Ceiling (double a) {
+        return System.Math.Ceiling(a);
+    }
+    public static System.Decimal Ceiling (Decimal d) {
+        return System.Math.Ceiling(d);
+    }
+    public static double Cos (double d) {
+        return System.Math.Cos(d);
+    }
+    public static double Cosh (double value) {
+        return System.Math.Cosh(value);
+    }
+    public static double Exp (double d) {
+        return System.Math.Exp(d);
+    }
+    public static double Floor (double d) {
+        return System.Math.Floor(d);
+    }
+    public static System.Decimal Floor (Decimal d) {
+        return System.Math.Floor(d);
+    }
+    public static double IEEERemainder (double x, double y) {
+        return System.Math.IEEERemainder(x, y);
+    }
+    public static double Log (double d) {
+        return System.Math.Log(d);
+    }
+    public static double Log (double a, double newBase) {
+        return System.Math.Log(a, newBase);
+    }
+    public static double Log10 (double d) {
+        return System.Math.Log10(d);
+    }
+    public static System.UInt16 Max (UInt16 val1, UInt16 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Single Max (Single val1, Single val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Int32 Max (Int32 val1, Int32 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static double Max (double val1, double val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Byte Max (Byte val1, Byte val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Decimal Max (Decimal val1, Decimal val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.UInt32 Max (UInt32 val1, UInt32 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.UInt64 Max (UInt64 val1, UInt64 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Int16 Max (Int16 val1, Int16 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.Int64 Max (Int64 val1, Int64 val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.SByte Max (SByte val1, SByte val2) {
+        return System.Math.Max(val1, val2);
+    }
+    public static System.UInt16 Min (UInt16 val1, UInt16 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static double Min (double val1, double val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Single Min (Single val1, Single val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.UInt64 Min (UInt64 val1, UInt64 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Decimal Min (Decimal val1, Decimal val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Int32 Min (Int32 val1, Int32 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Int16 Min (Int16 val1, Int16 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.UInt32 Min (UInt32 val1, UInt32 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Int64 Min (Int64 val1, Int64 val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.SByte Min (SByte val1, SByte val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static System.Byte Min (Byte val1, Byte val2) {
+        return System.Math.Min(val1, val2);
+    }
+    public static double Pow (double x, double y) {
+        return System.Math.Pow(x, y);
+    }
+    public static System.Decimal Round (Decimal d, MidpointRounding mode) {
+        return System.Math.Round(d, mode);
+    }
+    public static System.Decimal Round (Decimal d, Int32 decimals, MidpointRounding mode) {
+        return System.Math.Round(d, decimals, mode);
+    }
+    public static System.Decimal Round (Decimal d) {
+        return System.Math.Round(d);
+    }
+    public static System.Decimal Round (Decimal d, Int32 decimals) {
+        return System.Math.Round(d, decimals);
+    }
+    public static double Round (double value, MidpointRounding mode) {
+        return System.Math.Round(value, mode);
+    }
+    public static double Round (double value, Int32 digits, MidpointRounding mode) {
+        return System.Math.Round(value, digits, mode);
+    }
+    public static double Round (double a) {
+        return System.Math.Round(a);
+    }
+    public static double Round (double value, Int32 digits) {
+        return System.Math.Round(value, digits);
+    }
+    public static System.Int32 Sign (Int32 value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (Int64 value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (Single value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (Decimal value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (double value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (SByte value) {
+        return System.Math.Sign(value);
+    }
+    public static System.Int32 Sign (Int16 value) {
+        return System.Math.Sign(value);
+    }
+    public static double Sin (double a) {
+        return System.Math.Sin(a);
+    }
+    public static double Sinh (double value) {
+        return System.Math.Sinh(value);
+    }
+    public static double Sqrt (double d) {
+        return System.Math.Sqrt(d);
+    }
+    public static double Tan (double a) {
+        return System.Math.Tan(a);
+    }
+    public static double Tanh (double value) {
+        return System.Math.Tanh(value);
+    }
+    public static double Truncate (double d) {
+        return System.Math.Truncate(d);
+    }
+    public static System.Decimal Truncate (Decimal d) {
+        return System.Math.Truncate(d);
+    }
+}
