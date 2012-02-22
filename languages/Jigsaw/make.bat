ec5b3c5efe4a9ba13d9c8c9c5bffe7d5e427d2da
Doug Blank <dblank@brynmawr.edu>
2012-02-22 14:13:51 +0000

    Attempt to fix major problem in load/build

--- languages/Jigsaw/make.bat
+++ languages/Jigsaw/make.bat
@@ -1,3 +1,3 @@
 
-gmcs CalicoJigsaw.cs -lib:../../bin -lib:"c:\Program Files (x86)\GtkSharp\2.12\lib\gtk-sharp-2.0" -r:Jigsaw.exe -r:Calico.exe -r:gtk-sharp.dll -r:atk-sharp.dll -r:glib-sharp.dll -target:library
+dmcs CalicoJigsaw.cs -lib:../../bin -lib:"c:\Program Files (x86)\GtkSharp\2.12\lib\gtk-sharp-2.0" -r:Jigsaw.exe -r:Calico.exe -r:gtk-sharp.dll -r:atk-sharp.dll -r:glib-sharp.dll -target:library
 
