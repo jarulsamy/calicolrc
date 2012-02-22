22222873705e2ef1b195bc19a35300d19a28feb2
Doug Blank <dblank@brynmawr.edu>
2012-02-22 21:32:02 +0000

    Added Erf, Asinh, Acosh, and Atanh

--- modules/Math/Math.cs
+++ modules/Math/Math.cs
@@ -209,4 +209,38 @@ public class Math {
     public static System.Decimal Truncate (Decimal d) {
         return System.Math.Truncate(d);
     }
+
+    double Erf(double x) {
+      // constants
+      double a1 =  0.254829592;
+      double a2 = -0.284496736;
+      double a3 =  1.421413741;
+      double a4 = -1.453152027;
+      double a5 =  1.061405429;
+      double p  =  0.3275911;
+      
+      // Save the sign of x
+      int sign = 1;
+      if (x < 0)
+        sign = -1;
+      x = Abs(x);
+      
+      // A&S formula 7.1.26
+      double t = 1.0/(1.0 + p*x);
+      double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Exp(-x*x);
+      
+      return sign*y;
+    }
+
+    double Asinh(double x) {
+      return Log(x + Sqrt(Pow(x, 2) + 1));
+    }
+
+    double Acosh(double x) {
+      return Log(x + Sqrt(Pow(x, 2) - 1));
+    }
+
+    double Atanh(double x) {
+      return (Log(1 + x) - Log(1 - x))/2;
+    }
 }
