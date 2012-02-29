0d3b2528b98a681b3a7d48b31ed17922e59b75fd
Keith O'Hara <kohara@bard.edu>
2012-02-29 05:11:19 +0000

    Added example for configuring blob camera with mouse.

---
+++ examples/python/configureBlob.py
@@ -0,0 +1,22 @@
+from Myro import *
+from Graphics import *
+import Myro
+
+def configureBlob():
+    p = takePicture()
+    w = Window(256, 192)
+    p.draw(w)
+    p1 = getMouse()
+
+    while getMouseState() == "down":
+        p2 = getMouseNow()
+
+    r = Rectangle(p1, p1)
+    r.outline = None
+    r.color = Color(255, 255, 0, 64)
+    r.draw(w)
+    
+    print(p1, p2)
+
+    print (Myro.robot.set_blob_yuv(p, p1[0], p1[1], p2[0], p2[1]))
+    show(takePicture('blob'), "blob image")
