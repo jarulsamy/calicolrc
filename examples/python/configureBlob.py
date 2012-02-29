798f86d543f8b4ae5d12d2e2d256fc42d6401946
Keith O'Hara <kohara@bard.edu>
2012-02-29 13:00:21 +0000

    More interactive blob configuration

--- examples/python/configureBlob.py
+++ examples/python/configureBlob.py
@@ -2,21 +2,38 @@ from Myro import *
 from Graphics import *
 import Myro
 
+x1, y1, x2, y2 = None, None, None, None
+w = None
+r = None
+p = None
+
+def handleMouseUp(obj, event):
+    global x1,y1,x2,y2,p
+    x2,y2= event.x, event.y
+    print (Myro.robot.set_blob_yuv(p, x1, y1, x2, y2))
+    show(takePicture('blob'), "blob image")
+    x1, y1, x2, y2 = None, None, None, None
+
+def handleMouseMovement(obj, event):
+    global r,w,x1,y1,x2,y2
+    if r:
+        r.undraw()
+    if x1:
+        r = Rectangle((x1, y1), (event.x, event.y))
+        r.outline = None
+        r.color = Color(255, 255, 0, 64)
+        r.draw(w)
+
+
+def handleMouseDown(obj, event):
+    global x1,y1
+    x1,y1= event.x, event.y
+
 def configureBlob():
+    global p, w
     p = takePicture()
     w = Window(256, 192)
     p.draw(w)
-    p1 = getMouse()
-
-    while getMouseState() == "down":
-        p2 = getMouseNow()
-
-    r = Rectangle(p1, p2)
-    r.outline = None
-    r.color = Color(255, 255, 0, 64)
-    r.draw(w)
-    
-    print(p1, p2)
-
-    print (Myro.robot.set_blob_yuv(p, p1[0], p1[1], p2[0], p2[1]))
-    show(takePicture('blob'), "blob image")
+    w.onMouseMovement(handleMouseMovement)
+    w.onMouseUp(handleMouseUp)
+    w.onMouseDown(handleMouseDown)
