768be980407428f59c96824f2464e8efeabb90ba
Doug Blank <dblank@brynmawr.edu>
2012-03-05 14:31:41 +0000

    Rebuilt for windows, 2.0.1

--- examples/fsharp/Functions.fs
+++ examples/fsharp/Functions.fs
@@ -1,18 +1,18 @@
 // F# Function Examples
 
-module MyFunctions  // used in files to be able to load
+module MyFunctions;;  // used in files to be able to load
 
-open System // bring in Console
+open System;; // bring in Console
 
 // Create a function that will add 1 to a given number:
 
-let add1 x = x + 1
+let add1 x = x + 1;;
 
 // Create a function that will add two numbers together:
 
-let addem x y = x + y
+let addem x y = x + y;;
 
 // Call both of the functions, and display the results:
 
-Console.WriteLine(add1 4)
-Console.WriteLine(addem 42 8 )
+Console.WriteLine(add1 4);;
+Console.WriteLine(addem 42 8 );;
