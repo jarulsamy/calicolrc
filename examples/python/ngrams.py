db29e8191c6f7acee2e0d8c1237bfc35895af429
Doug Blank <dblank@brynmawr.edu>
2012-03-06 14:40:46 +0000

    Ngram graph, inspired by SIGCSE keynote

---
+++ examples/python/ngrams.py
@@ -0,0 +1,38 @@
+## Show what word follows what word in a text
+## Doug Blank
+
+from Myro import *
+import Graphics
+import string
+
+def process(filename):
+    fp = open(filename)
+    prev = None
+    ngram = {}
+    for line in fp:
+        remove = ".-?:/=(),[]#><{}\"'\\"
+        line = line.translate(string.maketrans(remove, " " * len(remove)))
+        line = line.translate(None, "0123456789")
+        line = line.lower()
+        for word in line.split():
+            if prev in ngram:
+                if word not in ngram[prev]:
+                    ngram[prev].append(word)
+            elif prev != None:
+                ngram[prev] = [word]
+            prev = word
+
+    g = Graphics.Graph()
+    win = Window(500, 500)
+    win.addScrollbars(5000, 500)
+    counts = list(reversed(list(set([len(ngram[key]) for key in ngram]))))
+    print(len(ngram))
+    for key in ngram.keys()[:20]:
+        #print(key, ngram[key])
+        #if 10 < len(ngram[key]) < 15:
+        for word in ngram[key]:
+            g.addEdge(key, word)
+    g.layout()
+    g.draw(win)
+
+process(pickAFile())
