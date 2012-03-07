5404770d5a5a5681fbcbc70f548ea6d9eacb5b43
Doug Blank <dblank@brynmawr.edu>
2012-03-07 02:54:43 +0000

    Rebuilt all for 2.0.2

--- Source/Calico/Main.cs
+++ Source/Calico/Main.cs
@@ -29,7 +29,7 @@ using System.Diagnostics;
 
 namespace Calico {
     class MainClass {
-        public static string Version = "2.0.1";
+        public static string Version = "2.0.2";
         public static bool IsLoadModules = true;
 
 		/*
