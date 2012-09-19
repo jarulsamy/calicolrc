f5023531e2b9742f9acec6e56e288b93eb19d716
Doug Blank <dblank@brynmawr.edu>
2012-09-19 15:20:57 +0000

    Bump version to 2.0.13, not released just trunk version

--- Source/Calico/Main.cs
+++ Source/Calico/Main.cs
@@ -29,7 +29,7 @@ using System.Diagnostics;
 
 namespace Calico {
     class MainClass {
-        public static string Version = "2.0.12";
+        public static string Version = "2.0.13";
         public static bool IsLoadLanguages = true;
 
 		/*
