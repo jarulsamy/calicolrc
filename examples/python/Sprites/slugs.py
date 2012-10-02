441cc0032cb735b1afbe87878d9d912294a57682
Doug Blank <dblank@brynmawr.edu>
2012-10-02 16:13:36 +0000

    Sprite example

---
+++ examples/python/Sprites/slugs.py
@@ -0,0 +1,11 @@
+
+from Sprites import *
+init()
+move(20)
+makeSprite()       # adds a second sprite
+flipHorizontal()
+speak("Hello, I am a slug!")
+transparent(0.0)
+speak("I'm invisible!")
+transparent(1.0)
+speak("I'm back!")
