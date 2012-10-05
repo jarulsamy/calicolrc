0c1c5af947de58320c8c3dc35a45af53be8aaeeb
Doug Blank <dblank@brynmawr.edu>
2012-10-05 14:01:53 +0000

    version 2.1.0

--- Source/Calico/Main.cs
+++ Source/Calico/Main.cs
@@ -29,7 +29,7 @@ using System.Diagnostics;
 
 namespace Calico {
     class MainClass {
-        public static string Version = "2.0.14";
+        public static string Version = "2.1.0";
         public static bool IsLoadLanguages = true;
 
 		/*
