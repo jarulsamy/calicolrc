82c0e7402b76f363f41b155f10bcbfed46574ee8
Doug Blank <dblank@brynmawr.edu>
2013-04-07 16:48:47 +0000

    Working on Picture.toBitmap...

--- modules/Graphics/Graphics.cs
+++ modules/Graphics/Graphics.cs
@@ -4061,7 +4061,7 @@ public static class Graphics
 		public System.Drawing.Bitmap toBitmap () {
 		    Gdk.Pixmap pixmap, mask;
 		    _pixbuf.RenderPixmapAndMask(out pixmap, out mask, 255);
-		    using (System.Drawing.Graphics graphics = Gtk.DotNet.Graphics.FromDrawable(pixmap)) {
+		    using (System.Drawing.Graphics graphics = Gtk.DotNet.Graphics.FromDrawable(mask)) {
 			return new System.Drawing.Bitmap(width, height, graphics);
 		    }
 		}
