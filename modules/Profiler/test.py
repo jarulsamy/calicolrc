34015cba31886ec0ae955778cad4c7fd4dfb0f63
Doug Blank <dblank@brynmawr.edu>
2013-04-20 17:57:46 +0000

    No longer needed

--- modules/Profiler/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.platform == 'cli':
-    import pyprof
-
-import nntplib
-server = nntplib.NNTP('news.gmane.org')
-print server.description('gmane.comp.python.general')
-server.quit()
