ba54c9a63b5f37604603f2e19ceb4136cc465f6e
Doug Blank <dblank@brynmawr.edu>
2013-04-22 17:11:13 +0000

    Not yet working; something not loaded?

--- examples/fsharp/Functions.fs
+++ examples/fsharp/Functions.fs
@@ -1,21 +1,21 @@
 // F# Function Examples
 
-module MyFunctions;;  // used in files to be able to load
+module MyFunctions  // used in files to be able to load
 
-open System;; // bring in Console
+open System; // bring in Console
 
 // Create a function that will add 1 to a given number:
 
-let add1 x = x + 1;;
+let add1 x = x + 1;
 
 // Create a function that will add two numbers together:
 
-let addem x y = x + y;;
+let addem x y = x + y;
 
 // Call both of the functions, and display the results:
 
-Console.WriteLine(add1 4);;
-Console.WriteLine(addem 42 8 );;
+Console.WriteLine(add1 4);
+Console.WriteLine(addem 42 8 );
 
 //let odd even n = even(odd n+1);;
 
@@ -26,6 +26,6 @@ let rec Even x =
    else Odd (x - 1)
 and Odd x =
    if x = 1 then true
-   else Even (x - 1);;
+   else Even (x - 1);
 
-Console.WriteLine(Odd(1));;
+Console.WriteLine(Odd(1));
--- examples/fsharp/graphics_test.fs
+++ examples/fsharp/graphics_test.fs
@@ -1,7 +1,8 @@
 module MyModule
 
-Graphics.Init()
-let win = Graphics.Window("Hello")
+Gtk.Application.Init()
+
+//let win = new Graphics.WindowClass("Hello")
 let line = new Graphics.Line(new Graphics.Point(0, 0),
                              new Graphics.Point(100, 100))
-line.draw(win)
+//line.draw(win)
