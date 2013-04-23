f8c0fe50ca03ca8a93505a92e7ee171f2c50f2dc
Doug Blank <dblank@brynmawr.edu>
2013-04-23 10:23:18 +0000

    Works, but locks up Process

--- examples/fsharp/graphics_test.fs
+++ examples/fsharp/graphics_test.fs
@@ -2,7 +2,8 @@ module MyModule
 
 Gtk.Application.Init()
 
-//let win = new Graphics.WindowClass("Hello")
+let win = new Graphics.WindowClass("Hello")
 let line = new Graphics.Line(new Graphics.Point(0, 0),
                              new Graphics.Point(100, 100))
-//line.draw(win)
+line.draw(win)
+Gtk.Application.Run()
--- examples/fsharp/mymodule.fs
+++ examples/fsharp/mymodule.fs
@@ -5,3 +5,4 @@ let win = Graphics.Window("Hello")
 let line = new Graphics.Line(new Graphics.Point(0, 0),
                              new Graphics.Point(100, 100))
 line.draw(win)
+Gtk.Application.Run()
