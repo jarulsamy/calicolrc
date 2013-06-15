340f1f5a8423308a448006f37c3cc9f8b4a7b818
Doug Blank <dblank@cs.brynmawr.edu>
2013-06-15 16:39:38 -0400

    Integrated Console with Calico functionality; added helpful commands

--- languages/Console/cash.py
+++ languages/Console/cash.py
@@ -12,6 +12,9 @@ import shutil
 ## -> returns a sequence [eg, returns a seq, or defines a generator]
 
 def cd(incoming, tty, args, flags):
+    """
+    cd - change directory
+    """
     directory = None
     if len(args) > 0:
         directory = args[0]
@@ -19,22 +22,34 @@ def cd(incoming, tty, args, flags):
         os.chdir(directory)
     else:
         os.chdir(os.path.expanduser("~"))
-    return []
+    return pwd([], False, [], [])
 
 def rm(incoming, tty, args, flags):
+    """
+    rm - remove file or folders
+    """
     os.remove(args[0])
     #os.rmdir() # removes an empty directory
     #shutil.rmtree() # removes directory and contents
     return []
 
 def cp(incoming, tty, args, flags):
+    """
+    cp - copy files and folders
+    """
     shutil.copy(args[0], args[1])
     return []
 
 def pwd(incoming, tty, args, flags):
+    """
+    pwd - print working directory
+    """
     return [os.getcwd()]
 
 def ls(incoming, tty, args, flags):
+    """
+    ls - list files
+    """
     pattern = "*"
     if incoming:
         data = match(pattern, incoming)
@@ -62,11 +77,17 @@ def ls(incoming, tty, args, flags):
         yield retval
 
 def grep(incoming, tty, args, flags):
+    """
+    grep - search for matches
+    """
     pattern = args[0]
     for i in match(pattern, incoming):
         yield i
 
 def more(incoming, tty, args, flags):
+    """
+    more - see output one page at a time
+    """
     count = 0
     lines = height - 2
     for i in incoming:
@@ -80,6 +101,9 @@ def more(incoming, tty, args, flags):
                 return
 
 def cat(incoming, tty, args, flags):
+    """
+    cat - concatenate files
+    """
     count = 0
     for i in incoming:
         count += 1
@@ -88,13 +112,55 @@ def cat(incoming, tty, args, flags):
         else:
             yield i
 
+def sort(incoming, tty, args, flags):
+    """
+    sort - sort data
+    """
+    return sorted(list(args) + list(incoming))
+
 def help_cmd(incoming, tty, args, flags):
+    """
+    help - get help on commands
+    """
     if len(args) == 0:
-        return commands.keys()
+        for command in sorted(commands.keys()):
+            yield commands[command].__doc__.strip()
     elif args[0] in commands:
-        return [commands[args[0]].__doc__]
+        yield commands[args[0]].__doc__.strip()
     else:
         raise Exception("I don't have help on '%s'" % args[0])
+
+def show(incoming, tty, args, flags):
+    """
+    show - show an image graphically
+    """
+    import Myro
+    if incoming:
+        for filename in incoming:
+            if os.path.isfile(filename):
+                pic = Myro.makePicture(filename)
+                Myro.show(pic, filename)
+    else:
+        for filename in args:
+            if os.path.isfile(filename):
+                pic = Myro.makePicture(filename)
+                Myro.show(pic, filename)
+    return []
+
+def open_cmd(incoming, tty, args, flags):
+    """
+    open - open a file in Calico
+    """
+    if incoming:
+        for filename in incoming:
+            if os.path.isfile(filename):
+                calico.Open(filename)
+    else:
+        for filename in args:
+            if os.path.isfile(filename):
+                calico.Open(filename)
+    return []
+
 
 ## ------------------------------------------------------------
 
@@ -167,6 +233,7 @@ def display(g):
 # Unix commands
 commands = {"ls": ls, "more": more, "cd": cd, "grep": grep,
             "cat": cat, "help": help_cmd, "pwd": pwd, "cp": cp,
-            "rm": rm, "less": more}
+            "rm": rm, "less": more, "show": show, "open": open_cmd,
+            "sort": sort}
 
 #execute("ls")
