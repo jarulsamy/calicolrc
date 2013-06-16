e2421ffe09f7c19fd12ff8a6c97acc60eeb8a8e3
Doug Blank <dblank@cs.brynmawr.edu>
2013-06-16 00:25:56 -0400

    Refactor, to allow arg/option parser

--- languages/Console/cash.py
+++ languages/Console/cash.py
@@ -6,13 +6,18 @@ import re
 import shutil
 
 ## ------------------------------------------------------------
+## Globals:
+
+lastcd = None
+
+## ------------------------------------------------------------
 ## Commands:
 
-## command(incoming-sequence, tty?, arguments, flags)
+## command(incoming-sequence, tty?, arguments)
 ## -> returns a sequence [eg, returns a seq, or defines a generator]
 ## __doc__ : first line, summary; rest for a complete help doc
 
-def cd(incoming, tty, args, flags):
+def cd(incoming, tty, args):
     """
     cd - change directory
 
@@ -21,68 +26,81 @@ def cd(incoming, tty, args, flags):
 
     see also: pwd, mkdir
     """
+    global lastcd
+    args, flags = splitArgs(args)
     directory = None
     if len(args) > 0:
         directory = args[0]
     # ---
+    if directory == "-":
+        if lastcd:
+            os.chdir(lastcd)
+        # else pass
     if directory:
         os.chdir(directory)
     else:
         os.chdir(os.path.expanduser("~"))
-    return pwd([], False, [], [])
+    lastcd = os.getcwd()
+    return [os.getcwd()]
 
-def rm(incoming, tty, args, flags):
+def rm(incoming, tty, args):
     """
     rm - remove file or folders
 
     Use rm to delete files.
     """
+    args, flags = splitArgs(args)
     os.remove(args[0])
     #shutil.rmtree() # removes directory and contents
     return []
 
-def mkdir(incoming, ttyp, args, flags):
+def mkdir(incoming, ttyp, args):
+    args, flags = splitArgs(args)
     for filename in args:
         os.makedirs(filename)
     return []
 
-def rmdir(incoming, tty, args, flags):
+def rmdir(incoming, tty, args):
     """
     rmdir - remove folders
 
     Use rmdir to delete folders.
     """
+    args, flags = splitArgs(args)
     for folder in args:
         os.rmdir(folder) # removes an empty directory
     return []
 
-def cp(incoming, tty, args, flags):
+def cp(incoming, tty, args):
     """
     cp - copy files and folders
 
     Use cp to copy files.
     """
+    args, flags = splitArgs(args)
     shutil.copy(args[0], args[1])
     return []
 
-def mv(incoming, tty, args, flags):
+def mv(incoming, tty, args):
     """
     mv - move files and folders
 
     Use mv to move files.
     """
+    args, flags = splitArgs(args)
     os.rename(args[0], args[1])
     return []
 
-def pwd(incoming, tty, args, flags):
+def pwd(incoming, tty, args):
     """
     pwd - print working directory
 
     Use pwd to see your current directory.
     """
+    args, flags = splitArgs(args)
     return [os.getcwd()]
 
-def ls(incoming, tty, args, flags):
+def ls(incoming, tty, args):
     """
     ls - list files
 
@@ -92,6 +110,7 @@ def ls(incoming, tty, args, flags):
        -a   see all
        -l   long format
     """
+    args, flags = splitArgs(args)
     if len(args) > 0:
         patterns = args
     else:
@@ -120,12 +139,13 @@ def ls(incoming, tty, args, flags):
     if retval:
         yield retval
 
-def grep(incoming, tty, args, flags):
+def grep(incoming, tty, args):
     """
     grep - search for matches
 
     Grep through arguments, searching for matches.
     """
+    args, flags = splitArgs(args)
     if incoming:
         pattern = args[0]
         for i in match(pattern, incoming):
@@ -134,10 +154,11 @@ def grep(incoming, tty, args, flags):
         pass # look in the contents of args
     return
 
-def more(incoming, tty, args, flags):
+def more(incoming, tty, args):
     """
     more - see output one page at a time
     """
+    args, flags = splitArgs(args)
     count = 0
     lines = height - 2
     if incoming:
@@ -154,10 +175,11 @@ def more(incoming, tty, args, flags):
     #else: # 
     #    for arg in args:
 
-def cat(incoming, tty, args, flags):
+def cat(incoming, tty, args):
     """
     cat - concatenate files
     """
+    args, flags = splitArgs(args)
     count = 0
     if incoming:
         for i in incoming:
@@ -177,16 +199,18 @@ def cat(incoming, tty, args, flags):
                 else:
                     yield i
 
-def sort(incoming, tty, args, flags):
+def sort(incoming, tty, args):
     """
     sort - sort data
     """
+    args, flags = splitArgs(args)
     return sorted(list(args) + list(incoming))
 
-def help_cmd(incoming, tty, args, flags):
+def help_cmd(incoming, tty, args):
     """
     help - get help on commands
     """
+    args, flags = splitArgs(args)
     if len(args) == 0:
         for command in sorted(commands.keys()):
             yield commands[command].__doc__.strip().split("\n")[0].strip()
@@ -195,10 +219,11 @@ def help_cmd(incoming, tty, args, flags):
     else:
         raise Exception("I don't have help on '%s'" % args[0])
 
-def show(incoming, tty, args, flags):
+def show(incoming, tty, args):
     """
     show - show an image graphically
     """
+    args, flags = splitArgs(args)
     import Myro
     if incoming:
         for filename in incoming:
@@ -212,10 +237,11 @@ def show(incoming, tty, args, flags):
                 Myro.show(pic, filename)
     return []
 
-def open_cmd(incoming, tty, args, flags):
+def open_cmd(incoming, tty, args):
     """
     open - open a file in Calico
     """
+    args, flags = splitArgs(args)
     if incoming:
         for filename in incoming:
             if os.path.isfile(filename):
@@ -226,7 +252,7 @@ def open_cmd(incoming, tty, args, flags):
                 calico.Open(filename)
     return []
 
-def exec_cmd(incoming, tty, args, flags):
+def exec_cmd(incoming, tty, args):
     """
     exec - execute a file in Calico
     """
@@ -250,10 +276,11 @@ def exec_cmd(incoming, tty, args, flags):
             calico.Execute(args[0], args[1])
     return []
 
-def eval_cmd(incoming, tty, args, flags):
+def eval_cmd(incoming, tty, args):
     """
     eval - evaluate text in Calico
     """
+    args, flags = splitArgs(args)
     if incoming:
         for item in incoming:
             parts = splitParts(item)
@@ -265,6 +292,13 @@ def eval_cmd(incoming, tty, args, flags):
         text = args[0]
         language = args[1]
         return [calico.Evaluate(text, language)]
+
+def echo(incoming, tty, args):
+    """
+    echo - display output
+    """
+    args, flags = splitArgs(args)
+    return [" ".join(args)]
 
 ## ------------------------------------------------------------
 
@@ -291,6 +325,22 @@ def split(arg_text):
     Split into command and args
     """
     return arg_text.split(" ", 1)
+
+def splitArgs(arg_list):
+    """
+    Split into command and args
+    """
+    args = []
+    flags = []
+    for arg in arg_list:
+        arg = arg.strip()
+        if arg in ["-", "--"]:
+            args.append(arg)
+        elif arg.startswith("-"):
+            flags.append(arg)
+        else:
+            args.append(arg)
+    return args, flags
 
 def splitParts(text):
     retval = []
@@ -341,19 +391,6 @@ def splitParts(text):
             retval.append(current)
     return retval
 
-def splitArgs(arg_text):
-    args = []
-    flags = []
-    for arg in splitParts(arg_text):
-        arg = arg.strip()
-        if arg in ["-", "--"]:
-            args.append(arg)
-        elif arg.startswith("-"):
-            flags.append(arg)
-        else:
-            args.append(arg)
-    return args, flags
-
 def parse(command_text):
     command_text = command_text.strip()
     if " " in command_text:
@@ -366,8 +403,8 @@ def parse(command_text):
         command = commands[command_name]
     else:
         raise Exception("No such command: '%s'. Try 'help'" % command_name)
-    args, flags = splitArgs(arg_text)
-    return command, args, flags
+    args = splitParts(arg_text)
+    return command, args
 
 def execute(calico, text):
     globals()["calico"] = calico
@@ -381,8 +418,8 @@ def execute(calico, text):
     count = 0
     for command_text in line:
         tty = (count == len(line) - 1) # pipe to a tty or not
-        command, args, flags = parse(command_text)
-        incoming = command(incoming, tty, args, flags)
+        command, args = parse(command_text)
+        incoming = command(incoming, tty, args)
         count += 1
     display(incoming)
 
@@ -390,11 +427,14 @@ def display(g):
     for item in g:
         print(item)
 
+# FIXME: head tail find pushd popd wget wc cal date du df uname cut plot printf time hostname id < >
+
 # Unix commands
 commands = {"ls": ls, "more": more, "cd": cd, "grep": grep,
             "cat": cat, "help": help_cmd, "pwd": pwd, "cp": cp,
             "rm": rm, "less": more, "show": show, "open": open_cmd,
             "sort": sort, "exec": exec_cmd, "eval": eval_cmd,
-            "mkdir": mkdir, "mv": mv, "rmdir": rmdir}
+            "mkdir": mkdir, "mv": mv, "rmdir": rmdir, "echo": echo, 
+            "edit": open_cmd}
 
 #execute("ls")
