121f2a9ac31722232c2334acfb9ea8bb268e2fbe
Keith O'Hara <kohara@bard.edu>
2013-07-04 16:12:45 -0400

    Humminbird/Processing example.

--- examples/python/Robots/Hummingbird/plotdistance.py
+++ examples/python/Robots/Hummingbird/plotdistance.py
@@ -13,10 +13,17 @@ size = 3
 
 def draw(o, e):
     global x
-    #pos = get("2","distance")
-    pos = get("4","light")
-    y = Processing.map(pos, 255, 0, 0, height())
-    ellipse(x, y, size, size)
+    y1 = Processing.map(get("2","distance"), 80, 0, 0, height())
+    y2 = Processing.map(get("4","light"), 255, 0, 0, height())
+    y3 = Processing.map(get("1","knob"), 255, 0, 0, height())
+
+    fill(255, 196, 0)
+    ellipse(x, y1, size, size)
+    fill(0, 255, 196)
+    ellipse(x, y2, size, size)
+    fill(255, 0, 196)
+    ellipse(x, y3, size, size)
+
     x = x + 1
     if x > width():
         x = 0
