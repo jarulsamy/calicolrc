558c0daf8a6c793edbc1b129ee66b3719735d13b
Doug Blank <doug.blank@gmail.com>
2013-07-09 15:20:19 -0400

    Run code in threads; test; rebuilt

--- examples/python/CS/events.py
+++ examples/python/CS/events.py
@@ -4,14 +4,19 @@ import Events
 
 Events.init()
 
+def message0(o, e):
+    Myro.wait(1)
+    print("Message 1 received by 0:", o, e)
+
 def message1(o, e):
     Myro.wait(1)
-    print("Message 1 received:", o, e)
+    print("Message 1 received by 1:", o, e)
 
 def message2(o, e):
     Myro.wait(1)
     print("Message 2 received:", o, e)
 
+Events.subscribe("message1", message0)
 Events.subscribe("message1", message1)
 Events.subscribe("message2", message2)
 
