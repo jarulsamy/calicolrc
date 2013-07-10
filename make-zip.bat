2981a8bbe8446808ea083b4ec04b91d1c84e00f9
Doug Blank <doug.blank@gmail.com>
2013-07-10 13:08:04 -0400

    Calico: updated make-zip.bat for Windows

--- make-zip.bat
+++ make-zip.bat
@@ -1,7 +1,7 @@
-C:\Users\dblank\Desktop\zip.exe -r calico-2.3.7-windows-all.zip Calico -x "Calico/papers/*" "Calico/bin/mac/*" "Calico/modules/*" "*/.svn/*" "Calico/src/*" "Calico/Source/*" "Calico/modules/*/*" "Calico/StartCalico" "Calico/StartCalico.app/*" "Calico/languages/Jigsaw/Jigsaw/*" "Calico/server/*" "Calico/plugins/*" "Calico/README.txt" "Calico/NOTES" "Calico/make*" "Calico/Makefile"
+C:\Users\dblank\Desktop\zip.exe -r Calico-2.3.9-windows-all.zip Calico -x "Calico/papers/*" "Calico/bin/mac/*" "Calico/modules/*" "*/.git/*" "Calico/src/*" "Calico/Source/*" "Calico/modules/*/*" "Calico/StartCalico" "Calico/StartCalico.app/*" "Calico/languages/Jigsaw/Jigsaw/*" "Calico/server/*" "Calico/plugins/*" "Calico/README.txt" "Calico/NOTES" "Calico/make*" "Calico/Makefile"
 
 
-C:\Users\dblank\Desktop\zip.exe -r -v -g calico-2.3.7-windows-all.zip "Calico/modules/*.dll" "Calico/modules/Myro/Robots/*.dll"
+C:\Users\dblank\Desktop\zip.exe -r -v -g Calico-2.3.9-windows-all.zip "Calico/modules/*.dll" "Calico/modules/Myro/Robots/*.dll"
  
 
-C:\Users\dblank\Desktop\zip.exe -r -v -g calico-2.3.7-windows-all.zip "Calico/languages/*/SyntaxModes/*.*" "Calico/languages/*/*.py" "Calico/languages/*/*/*.py"
+C:\Users\dblank\Desktop\zip.exe -r -v -g Calico-2.3.9-windows-all.zip "Calico/languages/*/SyntaxModes/*.*" "Calico/languages/*/*.py" "Calico/languages/*/*/*.py"
