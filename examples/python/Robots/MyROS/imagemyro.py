d941ed4035f67a3d809955a0b41cf693a73efd69
Keith O'Hara <kohara@bard.edu>
2013-07-10 23:16:35 -0400

    Myro usb_cam example.

---
+++ examples/python/Robots/MyROS/imagemyro.py
@@ -0,0 +1,14 @@
+# assumes usb_cam is running:
+# rosrun usb_cam usb_cam_node _pixel_format:=yuyv _image_width:=320 _image_height:=240
+
+from Myro import *
+makeRobot("ROSRobot")
+#makeRobot("ROSROBOT", "myip", "ROSip")
+#to access remote ROS process
+
+wait(5)
+
+for t in timer(30):
+  show(takePicture())
+
+
