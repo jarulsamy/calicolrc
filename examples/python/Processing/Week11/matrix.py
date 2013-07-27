6cb6069a40e359697072c1bf7729eb2900d5b0e5
Keith O'Hara <kohara@bard.edu>
2013-07-27 02:26:44 -0400

    Improved demo.

--- examples/python/Processing/Week11/matrix.py
+++ examples/python/Processing/Week11/matrix.py
@@ -1,25 +1,33 @@
 from Processing import *
 import string
-import random
-
-window(500, 500)
+from random import choice, uniform
+window(1680, 1050)
 window(displayWidth(), displayHeight())
 fullscreen()
 smooth()
 noFill()
 textFont("Courier")
-textSize(64)
-frameRate(40)
-stroke(0, 255, 24)
+textSize(28)
+frameRate(30)
+stroke(0, 255, 22)
+
+def keyPressed(o, e):
+    if (key() == key().space):
+        stroke(random(255), random(255), random(255))
+
+onKeyPressed += keyPressed
+
+choices = string.ascii_lowercase + string.uppercase + string.punctuation
+msg = []
+for i in range(2**11):
+    msg = msg + [choice(choices)]
 
 def draw(o, e):
     background(0)
-    msg = ""
-    choices = string.ascii_lowercase + string.uppercase + string.punctuation
-    for i in range(2**11):
-        msg = msg + random.choice(choices)
-    text(msg, 0, 0, width(), height())
-
+    for i in range(150):
+        idx = int(uniform(0, len(msg)))
+        msg[idx] = choice(choices)
+    text("".join(msg), 0, 0, width(), height())
 
 onLoop += draw
 loop()
