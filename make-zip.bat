d16a2c08e8ead6dc785a5682bd70e0b3795e2f40
Doug Blank <doug.blank@gmail.com>
2013-12-09 13:23:12 -0500

    Change location of zip.exe

--- make-zip.bat
+++ make-zip.bat
@@ -1,7 +1,9 @@
-C:\Users\dblank\Desktop\zip.exe -r Calico-2.4.3-windows-all.zip Calico -x "Calico/papers/*" "Calico/bin/mac/*" "Calico/modules/*" "*/.git/*" "Calico/src/*" "Calico/Source/*" "Calico/modules/*/*" "Calico/StartCalico" "Calico/StartCalico.app/*" "Calico/languages/Jigsaw/Jigsaw/*" "Calico/server/*" "Calico/plugins/*" "Calico/README.txt" "Calico/NOTES" "Calico/make*" "Calico/Makefile"
 
 
-C:\Users\dblank\Desktop\zip.exe -r -v -g Calico-2.4.3-windows-all.zip "Calico/modules/*.dll" "Calico/modules/Myro/Robots/*.dll"
+"C:\Program Files (x86)\GnuWin32\bin\zip.exe" -r Calico-2.4.3-windows-all.zip Calico -x "Calico/papers/*" "Calico/bin/mac/*" "Calico/modules/*" "*/.git/*" "Calico/src/*" "Calico/Source/*" "Calico/modules/*/*" "Calico/StartCalico" "Calico/StartCalico.app/*" "Calico/languages/Jigsaw/Jigsaw/*" "Calico/server/*" "Calico/plugins/*" "Calico/README.txt" "Calico/NOTES" "Calico/make*" "Calico/Makefile"
+
+
+'C:\Program Files (x86)\GnuWin32\bin\zip.exe' -r -v -g Calico-2.4.3-windows-all.zip "Calico/modules/*.dll" "Calico/modules/Myro/Robots/*.dll"
  
 
-C:\Users\dblank\Desktop\zip.exe -r -v -g Calico-2.4.3-windows-all.zip "Calico/languages/*/SyntaxModes/*.*" "Calico/languages/*/*.py" "Calico/languages/*/*/*.py"
+'C:\Program Files (x86)\GnuWin32\bin\zip.exe' -r -v -g Calico-2.4.3-windows-all.zip "Calico/languages/*/SyntaxModes/*.*" "Calico/languages/*/*.py" "Calico/languages/*/*/*.py"
