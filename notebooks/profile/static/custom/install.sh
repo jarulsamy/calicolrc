90f4b2c6e2826b82b94494d417d5bb7175684c4b
Doug Blank <dblank@cs.brynmawr.edu>
2014-07-27 01:17:33 -0400

    simple install for notebook extensions

---
+++ notebooks/profile/static/custom/install.sh
@@ -0,0 +1,12 @@
+## install file for getting Calico Noteboook Extensions
+
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/custom_example.js -o custom.css
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/custom.css 
+
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/bibtex.js
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/document-tools.js
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/cell-tools.js
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/typo.zip
+wget https://bitbucket.org/ipre/calico/raw/master/notebooks/profile/static/custom/spell-check.js
+
+unzip typo.zip
