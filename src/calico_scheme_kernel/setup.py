e00dc9ee0293a3199e72abb7f4c1faf7c261e266
Doug Blank <dblank@cs.brynmawr.edu>
2014-08-14 17:07:40 -0400

    kernels: python for running kernel is now the installing python; use 'pip install .' to install; also available on pypi

--- src/calico_scheme_kernel/setup.py
+++ src/calico_scheme_kernel/setup.py
@@ -1,12 +1,27 @@
-from distutils.core import setup
 from distutils.command.install import install
+from distutils.core import setup
+import os.path
+import json
 import sys
+
+kernel_json = {
+    "argv": [sys.executable, 
+	     "-m", "calico_scheme_kernel", 
+	     "-f", "{connection_file}"],
+    "display_name": "Scheme",
+    "language": "scheme"
+}
 
 class install_with_kernelspec(install):
     def run(self):
         install.run(self)
-        from IPython.kernel.kernelspec import install_kernel_spec
-        install_kernel_spec('kernelspec', 'calico_scheme_kernel', replace=True)
+        from IPython.kernel.kernelspec import KernelSpecManager
+        from IPython.utils.path import ensure_dir_exists
+        destdir = os.path.join(KernelSpecManager().user_kernel_dir, 
+                               'calico_scheme_kernel')
+        ensure_dir_exists(destdir)
+        with open(os.path.join(destdir, 'kernel.json'), 'w') as f:
+            json.dump(kernel_json, f, sort_keys=True)
 
 svem_flag = '--single-version-externally-managed'
 if svem_flag in sys.argv:
@@ -14,19 +29,20 @@ if svem_flag in sys.argv:
     sys.argv.remove(svem_flag)
 
 setup(name='calico_scheme_kernel',
-      version='0.0',
+      version='0.1',
       description='A Scheme kernel for IPython',
       url="https://bitbucket.org/ipre/calico/src/master/src/calico_scheme_kernel/",
-      long_description="A long description",
+      long_description="A Scheme kernel for IPython",
       author='Douglas Blank',
       author_email='doug.blank@gmail.com',
       py_modules=['calico_scheme_kernel'],
-      requires=["calico"],
+      install_requires=["calico"],
       cmdclass={'install': install_with_kernelspec},
       classifiers = [
           'Framework :: IPython',
           'License :: OSI Approved :: BSD License',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 2',
           'Topic :: System :: Shells',
       ]
 )
