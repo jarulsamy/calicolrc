bcc90d77d10d77ea4d01779807e4d1c064093454
Doug Blank <dblank@cs.brynmawr.edu>
2014-08-17 22:45:25 -0400

    calico.graphics: added more shapes

--- src/calico/graphics.py
+++ src/calico/graphics.py
@@ -97,3 +97,117 @@ class Text(Shape):
 
     def _add(self, drawing):
         drawing.add(drawing.text(text=self.text, insert=self.start, **self.extra))
+
+class Rectangle(Shape):
+    def __init__(self, start=(0,0), size=(1,1), rx=None, ry=None, **extra):
+        super(Rectangle, self).__init__()
+        self.start = start
+        self.size = size
+        self.rx = rx
+        self.ry = ry
+        self.extra = extra
+
+    def moveTo(self, start):
+        self.start = start
+
+    def move(self, (delta_x, delta_y)):
+        self.start = self.start[0] + delta_x, self.start[1] + delta_y
+
+    def _add(self, drawing):
+        drawing.rect(insert=self.start, size=self.size, rx=self.rx, ry=self.ry, **self.extra)
+
+class Ellipse(Shape):
+    def __init__(self, center=(0,0), radii=(1,1), **extra):
+        super(Ellipse, self).__init__()
+        self.center = center
+        self.radii = radii
+        self.extra = extra
+
+    def moveTo(self, center):
+        self.center = center
+
+    def move(self, (delta_x, delta_y)):
+        self.center = self.center[0] + delta_x, self.center[1] + delta_y
+
+    def _add(self, drawing):
+        drawing.ellipse(center=self.center, r=self.radii, **self.extra)
+
+class Polyline(Shape):
+    def __init__(self, points=[], **extra):
+        super(Polyline, self).__init__()
+        self.points = points
+        self.extra = extra
+
+    def moveTo(self, start):
+        diff_x = start[0] - points[0][0]
+        diff_y = start[1] - points[0][1]
+        for p in range(len(self.points)):
+            point[i] = point[i][0] + diff_x, point[i][1] + diff_y
+
+    def move(self, (delta_x, delta_y)):
+        for p in range(len(self.points)):
+            point[i] = point[i][0] + delta_x, point[i][1] + delta_y
+
+    def _add(self, drawing):
+        drawing.polyline(points=self.points, **self.extra)
+
+class Polygon(Shape):
+    def __init__(self, points=[], **extra):
+        super(Polygon, self).__init__()
+        self.points = points
+        self.extra = extra
+
+    def moveTo(self, start):
+        diff_x = start[0] - points[0][0]
+        diff_y = start[1] - points[0][1]
+        for p in range(len(self.points)):
+            point[i] = point[i][0] + diff_x, point[i][1] + diff_y
+
+    def move(self, (delta_x, delta_y)):
+        for p in range(len(self.points)):
+            point[i] = point[i][0] + delta_x, point[i][1] + delta_y
+
+    def _add(self, drawing):
+        drawing.polygon(points=self.points, **self.extra)
+
+class Image(Shape):
+    def __init__(self, href, start=(0,0), **extra):
+        super(Image, self).__init__()
+        self.href = href
+        self.start = start
+        self.extra = extra
+
+    def moveTo(self, start):
+        self.start = start
+
+    def move(self, (delta_x, delta_y)):
+        self.start = self.start[0] + delta_x, self.start[1] + delta_y
+
+    def _add(self, drawing):
+        drawing.image(href, insert=self.start, size=self.size, **self.extra)
+
+#g(**extra) # Group
+#symbol(**extra)
+#svg(insert=None, size=None, **extra)
+#use(href, insert=None, size=None, **extra)
+#a(href, target='_blank', **extra) # Link
+#marker(insert=None, size=None, orient=None, **extra)
+#script(href=None, content='', **extra)
+#style(content='', **extra)
+#linearGradient(start=None, end=None, inherit=None, **extra)
+#radialGradient(center=None, r=None, focal=None, inherit=None, **extra)
+#mask(start=None, size=None, **extra)
+#clipPath(**extra)
+#set(element=None, **extra)
+
+#tspan(text, insert=None, x=[], y=[], dx=[], dy=[], rotate=[], **extra) # TextSpan
+#tref(element, **extra)
+#textPath(path, text, startOffset=None, method='align', spacing='exact', **extra)
+#textArea(text=None, insert=None, size=None, **extra)
+#path(d=None, **extra)
+
+#animate(element=None, **extra)
+#animateColor(element=None, **extra)
+#animateMotion(element=None, **extra)
+#animateTransform(transform, element=None, **extra)
+#filter(start=None, size=None, resolution=None, inherit=None, **extra)
