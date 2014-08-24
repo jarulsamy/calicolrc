ff3981a521da5a460c49087ebf18568f67975922
Doug Blank <dblank@cs.brynmawr.edu>
2014-08-24 13:58:35 -0400

    calico_scheme_kernel: hook up help

--- src/calico_scheme_kernel/calico_scheme_kernel.py
+++ src/calico_scheme_kernel/calico_scheme_kernel.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from calico import MagicKernel
 import calico.scheme
 import os
+import logging
 
 class CalicoSchemeKernel(MagicKernel):
     implementation = 'Scheme'
@@ -11,9 +12,166 @@ class CalicoSchemeKernel(MagicKernel):
     language_version = '3.0'
     banner = "Calico Scheme"
 
-    def get_usage(self):
-        return "This is a usage statement."
+    def __init__(self, *args, **kwargs):
+        super(CalicoSchemeKernel, self).__init__(*args, **kwargs)
+        self.log.setLevel(logging.INFO)
 
+    def get_usage(self):
+        return """Calico Scheme 
+=========================================
+
+Calico Scheme offers a combination of convenient shell features,
+special commands and a history mechanism for both input (command
+history) and output (results caching, similar to Mathematica). 
+
+MAIN FEATURES
+-------------
+
+* Magic commands: type %magic for information on the magic subsystem.
+
+* Dynamic object information:
+
+  Typing ?word prints detailed information about an object.  If
+  certain strings in the object are too long (docstrings, code, etc.) they get
+  snipped in the center for brevity.
+
+  Typing ??word gives access to the full information without
+  snipping long strings. Long strings are sent to the screen through the less
+  pager if longer than the screen, printed otherwise.
+
+* Completion in the local namespace, by typing TAB at the prompt.
+
+  At any time, hitting tab will complete any available commands or
+  variable names, and show you a list of the possible completions if there's
+  no unambiguous one. It will also complete filenames in the current directory.
+
+  This feature requires the readline and rlcomplete modules, so it won't work
+  if your system lacks readline support (such as under Windows).
+
+* Search previous command history in two ways (also requires readline):
+
+  - Start typing, and then use Ctrl-p (previous,up) and Ctrl-n (next,down) to
+    search through only the history items that match what you've typed so
+    far. If you use Ctrl-p/Ctrl-n at a blank prompt, they just behave like
+    normal arrow keys.
+
+  - Hit Ctrl-r: opens a search prompt. Begin typing and the system searches
+    your history for lines that match what you've typed so far, completing as
+    much as it can.
+
+  - %hist: search history by index (this does *not* require readline).
+
+* Persistent command history across sessions.
+
+* Logging of input with the ability to save and restore a working session.
+
+* System escape with !. Typing !ls will run 'ls' in the current directory.
+
+* Input caching system:
+
+  Offers numbered prompts (In/Out) with input and output caching. All
+  input is saved and can be retrieved as variables (besides the usual arrow
+  key recall).
+
+  The following GLOBAL variables always exist (so don't overwrite them!):
+  _i: stores previous input.
+  _ii: next previous.
+  _iii: next-next previous.
+  _ih : a list of all input _ih[n] is the input from line n.
+
+  Additionally, global variables named _i<n> are dynamically created (<n>
+  being the prompt counter), such that _i<n> == _ih[<n>]
+
+  For example, what you typed at prompt 14 is available as _i14 and _ih[14].
+
+* Output caching system:
+
+  For output that is returned from actions, a system similar to the input
+  cache exists but using _ instead of _i. Only actions that produce a result
+  (NOT assignments, for example) are cached. If you are familiar with
+  Mathematica, Jupyter's _ variables behave exactly like Mathematica's %
+  variables.
+
+  The following GLOBAL variables always exist (so don't overwrite them!):
+  _ (one underscore): previous output.
+  __ (two underscores): next previous.
+  ___ (three underscores): next-next previous.
+
+  Global variables named _<n> are dynamically created (<n> being the prompt
+  counter), such that the result of output <n> is always available as _<n>.
+
+  Finally, a global dictionary named _oh exists with entries for all lines
+  which generated output.
+
+* Directory history:
+
+  Your history of visited directories is kept in the global list _dh, and the
+  magic %cd command can be used to go to any entry in that list.
+"""
+
+    def get_complete(self, code, start, end):
+        token = ""
+        current = end - 1
+        while current >= 0:
+            if code[current] == "(":
+                return (token, current + 1, end)
+            token = code[current] + token
+            current -= 1
+        return (token, start, end)
+
+    def do_complete(self, code, cursor_pos):
+        # Code completion
+        # Parameters:	
+        # code (str) – The code already present
+        # cursor_pos (int) – The position in the code where completion is requested
+        token, start, end = self.get_complete(code, 0, cursor_pos)
+        content = {
+            'matches' : [],
+            'cursor_start' : start,
+            'cursor_end' : end,
+            'metadata' : {},
+            'status' : 'ok'
+        }
+        start = cursor_pos - len(code)
+        # from the environment:
+        slist = calico.scheme.execute_string_rm("(dir)")
+        if not calico.scheme.exception_q(slist):
+            for item in slist:
+                item_str = str(item)
+                if item_str.startswith(token):
+                    content["matches"].append(item_str)
+        # special forms:
+        for item in ["define", "define!", "func", "callback", "if",
+                     "help", "define-syntax", "begin", "lambda", "trace-lambda",
+                     "try", "catch", "finally", "raise", "choose"]:
+            if item.startswith(token):
+                content["matches"].append(item)
+        # from magics:
+        for magic in self.magics.values():
+            for help_line in magic.help_lines:
+                item = help_line.split("-", 1)[0].strip().split(" ", 1)[0]
+                if item.startswith(token):
+                    content["matches"].append(item)
+        return content
+
+    def do_inspect(self, code, cursor_pos, detail_level=0):
+        # Object introspection
+        content = {'status': 'aborted', 'data': dict(), 'metadata': dict()}
+        docstring = "Help!"
+        if docstring:
+            content["data"] = self.formatter(docstring)
+            content["status"] = "ok"
+        return content
+
+    def help_patterns(self):
+        # Longest first:
+        return [
+            ("^\?\?(.*)$", 2,
+             "??item - get detailed help on item"), # "??code"
+            ("^\?(.*)$", 1, 
+             "?item - get help on item"),   # "?code"
+        ]
+        
     def set_variable(self, name, value):
         """
         Set a variable in the kernel language.
@@ -21,7 +179,9 @@ class CalicoSchemeKernel(MagicKernel):
         pass
 
     def get_help_on(self, expr, level):
-        return "Sorry, no help is available on '%s'." % expr
+        result = calico.scheme.execute_string_rm("(help %s)" % expr)
+        if not calico.scheme.exception_q(result):
+            return result
 
     def repr(self, item):
         if isinstance(item, list): # a scheme vector
