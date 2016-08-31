7108bbd3ff32b751437202c1bc87f39a0101d6f3
Roby Velez <roby.velez@gmail.com>
2016-08-31 14:19:21 -0600

    versionof info session

---
+++ mono/bin/mono-configuration-crypto.bat
@@ -0,0 +1,1 @@
+@"C:\PROGRA~2\MONO-2~1.8\bin\mono.exe" %MONO_OPTIONS% "C:\PROGRA~2\MONO-2~1.8\lib\mono\mono-configuration-crypto\4.0\mono-configuration-crypto.exe" %*
---
+++ mono/bin/soapsuds.bat
@@ -0,0 +1,1 @@
+@"C:\PROGRA~2\MONO-2~1.8\bin\mono.exe" %MONO_OPTIONS% "C:\PROGRA~2\MONO-2~1.8\lib\mono\4.0\soapsuds.exe" %*
